e94ed4e2ce27c3d08d25146d0c2607235fe9a9d5
student-PiyushMishra <student.piyushmishra@gmail.com>
2025-01-05 20:17:45 +0530

    bhai vercel to abhi humse na chalega

--- index.js
+++ index.js
@@ -3,69 +3,51 @@ const app = express();
 const ejs = require('ejs');
 const path = require("path");
 const fs = require("fs");
-const { log } = require("console");
-const { nextTick } = require("process");
 
-app.set("view engine","ejs");
-app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     fs.readdir("./texts", function (err, files) {
         if (err) {
-          console.error("Error reading directory:", err);
-          return res.status(500).send("Error reading directory");
+            console.error("Error reading directory:", err);
+            return res.status(500).send("Error reading directory");
         }
         res.render('index', { files: files });
-      });
-      
+    });
+
 })
 
-app.post("/create",(req,res)=>{
-    fs.writeFile(`./texts/${(req.body.title).split(" ").join("")}.txt`,req.body.details,function(err){
+app.post("/create", (req, res) => {
+    fs.writeFile(`./texts/${(req.body.title).split(" ").join("")}.txt`, req.body.details, function (err) {
         res.redirect("/")
     })
 })
 
-app.get("/delete/:filename",(req,res,next)=>{
-    fs.unlink(`./texts/${req.params.filename}`,function(err){
-        if(err) console.error(err);
+app.get("/delete/:filename", (req, res, next) => {
+    fs.unlink(`./texts/${req.params.filename}`, function (err) {
+        if (err) console.error(err);
         else res.redirect("/");
     })
 })
 
-app.get('/edit/:filename',(req,res)=>{
-    res.render("edit",{fileName:req.params.filename})
+app.get('/edit/:filename', (req, res) => {
+    res.render("edit", { fileName: req.params.filename })
 })
 
-app.post("/edit",(req,res)=>{
-    fs.rename(`./texts/${req.body.previous}`,`./texts/${(req.body.new).split(" ").join("")}.txt`,function(){
+app.post("/edit", (req, res) => {
+    fs.rename(`./texts/${req.body.previous}`, `./texts/${(req.body.new).split(" ").join("")}.txt`, function () {
         res.redirect("/");
     })
 })
 
-app.get("/file/:filename",(req,res)=>{
-    fs.readFile(`./texts/${req.params.filename}`,'utf-8',function(err,filedata){
-        res.render('show',{fileName:req.params.filename,fileData:filedata})
+app.get("/file/:filename", (req, res) => {
+    fs.readFile(`./texts/${req.params.filename}`, 'utf-8', function (err, filedata) {
+        res.render('show', { fileName: req.params.filename, fileData: filedata })
     });
 })
 
-const directoryPath = path.join(__dirname, "texts");
-console.log("Directory path:", directoryPath);
-
-fs.readdir(directoryPath, function(err, files) {
-  if (err) {
-    console.error("Error reading directory:", err);
-    return res.status(500).send("Error reading directory");
-  }
-  res.render("index", { files: files });
-});
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
-
+app.listen(3000);
